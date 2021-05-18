ffcf799974c9e9ed781e3c0959f46d92f1882376
Clarissa P <clarissapamonicutt@gmail.com>
2021-05-17 23:54:08 -0700

    commit to new branch

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Create a constructor function that creates an object associated with each product, and has the following properties:
+  // Name of the product
+  // File path of image
+  // Times the image has been shown
+
+
+
+  const Item();
