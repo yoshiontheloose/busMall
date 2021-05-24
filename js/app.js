bfa9c35a433c1dd586dc8019ee1af3f4f9f5b169
Clarissa P <clarissapamonicutt@gmail.com>
2021-05-23 23:41:08 -0700

    not pretty, but finished

--- js/app.js
+++ js/app.js
@@ -3,13 +3,18 @@
 // -------Global Variables-------(const=constant)
 
 const resultsUlElem = document.getElementById('product_clicks');
-const productImageSection = document.getElementById('all_products'); //listener goes on
+const productImageSection = document.getElementById('all_products');
 const leftImage = document.getElementById('left_image');
 const centerImage = document.getElementById('center_image');
 const rightImage = document.getElementById('right_image');
 const leftProductText = document.getElementById('left_text');
 const centerProductText = document.getElementById('center_text');
 const rightProductText = document.getElementById('right_text');
+
+//arrays for data and labels
+const productData = [];
+const productLabels = [];
+const productShown = [];
 
 let voteCounter = 0;
 
@@ -32,6 +37,8 @@ function Product(name, imagePath, timesShown, timesVoted) {
 // Array of all the Products
 Product.allProducts = [];
 
+
+
 // Prototype is a function to execute code from objects 
 // Make a prototype to use with the render function below
 Product.prototype.renderProduct = function(productText, productImage) {
@@ -48,18 +55,20 @@ function renderProducts(leftProduct, centerProduct, rightProduct) {
 
 //put results on html, section "product clicks"
 function renderResults() {
-  //clear out inner html of "product clicks"
+    //clear out inner html of "product clicks"
   resultsUlElem.innerHTML = '';
-  //add an h2 element with text content of results
+    //add an h2 element with text content of results
   let createH2Elem = document.createElement('h2');
   createH2Elem.textContent = 'Results';
   resultsUlElem.appendChild(createH2Elem);
+  
   //loop through the all products array and render an li for each product
   for (let i=0; i < Product.allProducts.length; i++) {
     let currentItem = Product.allProducts[i]
     let liElement = document.createElement('li');
-  //"image name" : "x amount" of votes and timesShown
-    liElement.textContent = `${currentItem.name} : amount of votes ${currentItem.timesVoted} Times Shown : ${currentItem.timesShown} `
+  
+    //"image name" : "x amount" of votes and timesShown
+    liElement.textContent = `${currentItem.name} Votes: ${currentItem.timesVoted} Times Shown: ${currentItem.timesShown} `
     resultsUlElem.appendChild(liElement);
   }
 }
@@ -94,18 +103,16 @@ function selectProducts() {
 
 }
 
+
 //begin chart
 const displayChart = function() {
   const resultsChart = document.getElementById('resultsChart').getContext('2d');
 
-  //arrays for data and labels
-  const productData = [];
-  const productLabels = [];
-
-  //go through products
+  //iterate through array of Product.allproducts
   for (let product of Product.allProducts) {
     productData.push(product.timesVoted);
-    productLabels.push(product.timesShown);
+    productShown.push(product.timesShown);
+    productLabels.push(product.name);
   }
 
     const myChart = new Chart(resultsChart, {
@@ -132,6 +139,9 @@ const displayChart = function() {
                   'rgba(255, 159, 64, 1)'
               ],
               borderWidth: 1
+          }, {
+            label: 'Times Shown',
+            data: productShown,
           }]
       },
       options: {
@@ -145,47 +155,99 @@ const displayChart = function() {
 
 }
 
-
 // listener for image clicks
-function productClick(e) {
-  let userClick = e.target
+function productClick(event) {
+  const userClick = event.target
   console.log('listening');
-
+  console.log('Vote counter = ' + voteCounter)
   if (voteCounter < 25) {
     if (userClick === leftImage || userClick === centerImage || userClick === rightImage) {
-      voteCounter++;
-
       if (userClick === leftImage) {
         currentLeftProduct.timesVoted++;
       }
       else if (userClick === centerImage) {
-        currentCenterProduct.votes++;
+        currentCenterProduct.timesVoted++;
       }
       else {
-        currentRightProduct.votes++
+        currentRightProduct.timesVoted++
       }
+      voteCounter++;
+      saveLocalData(voteCounter,'votes')
       currentLeftProduct.timesShown++;
       currentCenterProduct.timesShown++;
       currentRightProduct.timesShown++;
-
-      selectProducts();
-      renderProducts(currentLeftProduct, currentCenterProduct, currentRightProduct);
-    }
-    else {
-      alert(`Click this image to pick a product`);
+      saveLocalData(Product.allProducts,'results');
+      
     }
   }
-  else {
+  if (voteCounter === 25) {
+    console.log("Max votes reached")
     productImageSection.removeEventListener('click', productClick);
     renderResults();
     displayChart();
+    saveLocalData(Product.allProducts,'results');
+  }
+  selectProducts();
+  renderProducts(currentLeftProduct, currentCenterProduct, currentRightProduct);
+}
+
+
+
+// storage
+function saveLocalData(objectToSave, dataString) {
+  // pass in the object to save with a name to identify it
+  console.log('Saving ' + objectToSave + ' to json ' + dataString);
+  let stringifiedObjects = JSON.stringify(objectToSave)
+  localStorage.setItem(dataString, stringifiedObjects);
+}
+
+function loadLocalData(dataString) {
+  // pass in the object to load with the identifier defined when saving
+  let productStorage = localStorage.getItem(dataString);
+  //console.log(productStorage);
+  if (dataString === 'results') {
+    // if the loaded data string is the results json file load it into an object
+    console.log('loading result json File');
+    let loadedProducts = JSON.parse(productStorage)
+    for (let products of loadedProducts) {
+      let newProduct = new Product(products.name, products.imagePath);
+      newProduct.timesVoted = products.timesVoted;
+      newProduct.timesShown = products.timesShown;
+    } console.log(Product.allProducts);
+  }
+  else if (dataString === 'votes'){
+    // if the loaded data string is the votes json load it into the voteCounter variable
+    console.log('Loading vote json file');
+    voteCounter = JSON.parse(productStorage);
+    if (voteCounter === 25) {
+      console.log("Max votes reached")
+      // prompt the user if they want to load the vote counter to see the previous session results
+      if (confirm('Previous voting session found, do you want to load the results?')) {
+        console.log("User chosen to load results, disabling on click event to stop voting")
+        productImageSection.removeEventListener('click', productClick);
+        loadLocalData('results');
+        renderResults();
+        displayChart();
+        //saveLocalData(Product.allProducts,'results');
+      }
+      else{
+        console.log("Use chosen not to load session");
+        // if the user chose not to load the results, set the voteCounter back to 0 to start a new session
+        voteCounter = 0
+        makeProducts();
+        productImageSection.addEventListener('click', productClick);
+      }
+  }
+  else {
+    console.log("No previous session data found, starting a new vote");
+    // if no previous session data is found pick our products and display them to vote
+    makeProducts();
+    productImageSection.addEventListener('click', productClick);
+  }
   }
 }
-//re-add listener
-productImageSection.addEventListener('click', productClick);
-
   
-
+function makeProducts() {
 new Product('R2D2 Suitcase', './img/bag.jpg');
 new Product('Banana Slicer', './img/banana.jpg');
 new Product('Smart Roll TP Stand', './img/bathroom.jpg');
@@ -205,6 +267,7 @@ new Product('Star Wars Sleeping Bag', './img/tauntaun.jpg');
 new Product('Ethically sourced Unicorn Meat', './img/unicorn.jpg');
 new Product('Self-Watering Watering Can', './img/water-can.jpg');
 new Product('Ergonomic Wine Drinking Apparatus', './img/wine-glass.jpg');
-
+}
+loadLocalData('votes');
 selectProducts();
 renderProducts(currentLeftProduct, currentCenterProduct, currentRightProduct);
